30dde89c79dfe18b1945365c6b09243eb61d94ae
panterz <panagiwtis.terzis@gmail.com>
2015-12-09 17:44:23 +0000

    added precommit hooks and lint

--- dist/common/convert.common.js
+++ dist/common/convert.common.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _jquery = require("jquery");
+var _jquery = require('jquery');
 
 var _jquery2 = _interopRequireDefault(_jquery);
 
@@ -52,33 +52,33 @@ class Convertor {
         var c = this;
         switch (type) {
             case 'text':
-                field["label"] = html.find('input[name="label"]').val();
-                field["prefix"] = html.find('input[name="prefix"]').val();
-                field["placeholder"] = html.find('input[name="placeholder"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
-                field["persistent"] = html.find('input[name="persistent"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.prefix = html.find('input[name="prefix"]').val();
+                field.placeholder = html.find('input[name="placeholder"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
+                field.persistent = html.find('input[name="persistent"]').is(':checked');
                 field["max-chars"] = html.find('input[name="max-chars"]').val();
                 break;
             case 'textarea':
-                field["label"] = html.find('input[name="label"]').val();
-                field["placeholder"] = html.find('input[name="placeholder"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
-                field["persistent"] = html.find('input[name="persistent"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.placeholder = html.find('input[name="placeholder"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
+                field.persistent = html.find('input[name="persistent"]').is(':checked');
                 break;
             case 'range':
-                field["label"] = html.find('input[name="label"]').val();
-                field["placeholder"] = html.find('input[name="placeholder"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
-                field["persistent"] = html.find('input[name="persistent"]').is(':checked');
-                field["step"] = html.find('input[name="step"]').val();
-                field["min"] = html.find('input[name="min"]').val();
-                field["max"] = html.find('input[name="max"]').val();
+                field.label = html.find('input[name="label"]').val();
+                field.placeholder = html.find('input[name="placeholder"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
+                field.persistent = html.find('input[name="persistent"]').is(':checked');
+                field.step = html.find('input[name="step"]').val();
+                field.min = html.find('input[name="min"]').val();
+                field.max = html.find('input[name="max"]').val();
                 break;
             case 'checkbox':
-                field["label"] = html.find('input[name="label"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
-                field["persistent"] = html.find('input[name="persistent"]').is(':checked');
-                field["other"] = html.find('input[name="other"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
+                field.persistent = html.find('input[name="persistent"]').is(':checked');
+                field.other = html.find('input[name="other"]').is(':checked');
                 var checkboxes = [];
 
                 html.find('input[name="' + id + '"]').each(function (event) {
@@ -92,13 +92,13 @@ class Convertor {
                         checkboxes.push((0, _jquery2.default)(this).val());
                     }
                 });
-                field["checkboxes"] = checkboxes;
+                field.checkboxes = checkboxes;
                 break;
             case 'radio':
-                field["label"] = html.find('input[name="label"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
-                field["persistent"] = html.find('input[name="persistent"]').is(':checked');
-                field["other"] = html.find('input[name="other"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
+                field.persistent = html.find('input[name="persistent"]').is(':checked');
+                field.other = html.find('input[name="other"]').is(':checked');
                 var radios = [];
 
                 //go through each radio element
@@ -114,12 +114,12 @@ class Convertor {
                         radios.push((0, _jquery2.default)(this).val());
                     }
                 });
-                field["radios"] = radios;
+                field.radios = radios;
                 break;
             case 'select':
-                field["label"] = html.find('input[name="label"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
-                field["persistent"] = html.find('input[name="persistent"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
+                field.persistent = html.find('input[name="persistent"]').is(':checked');
                 var options = [];
                 html.find('input[name="' + id + '"]').each(function (event) {
                     var $img = (0, _jquery2.default)(this).closest(".form-inline").find("img");
@@ -133,35 +133,35 @@ class Convertor {
                         options.push((0, _jquery2.default)(this).val());
                     }
                 });
-                field["options"] = options;
+                field.options = options;
                 break;
             case 'dtree':
                 var $a = html.find('a');
-                field["label"] = html.find('input[name="label"]').val();
-                field["filename"] = $a.text();
+                field.label = html.find('input[name="label"]').val();
+                field.filename = $a.text();
                 break;
             case 'image':
-                field["label"] = html.find('input[name="label"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
                 field["multi-image"] = html.find('input[name="multi-image"]').is(':checked');
-                field["los"] = html.find('input[name="los"]').is(':checked');
-                field["blur"] = html.find('input[name="blur"]').val();
+                field.los = html.find('input[name="los"]').is(':checked');
+                field.blur = html.find('input[name="blur"]').val();
                 break;
             case 'audio':
-                field["label"] = html.find('input[name="label"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
                 break;
             case 'gps':
-                field["label"] = html.find('input[name="label"]').val();
-                field["required"] = html.find('input[name="required"]').is(':checked');
+                field.label = html.find('input[name="label"]').val();
+                field.required = html.find('input[name="required"]').is(':checked');
                 field["gps-background"] = html.find('input[name="gps-background"]').is(':checked');
                 break;
             case 'warning':
-                field["label"] = html.find('input[name="label"]').val();
-                field["placeholder"] = html.find('textarea').val();
+                field.label = html.find('input[name="label"]').val();
+                field.placeholder = html.find('textarea').val();
                 break;
             case 'section':
-                field["label"] = html.find('input[name="label"]').val();
+                field.label = html.find('input[name="label"]').val();
                 break;
             case undefined:
                 break;
@@ -357,37 +357,38 @@ class Convertor {
             var $this = (0, _jquery2.default)(this);
             var id = $this.attr("id");
             var type = $this.attr("id").split("-")[1];
+            var $input;
             form[id] = {};
             switch (type) {
                 case 'text':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('input');
-                    form[id]["prefix"] = $input.val();
-                    form[id]["placeholder"] = $input.prop("placeholder");
-                    form[id]["required"] = $input.prop("required");
-                    form[id]["persistent"] = $this.data("persistent");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('input');
+                    form[id].prefix = $input.val();
+                    form[id].placeholder = $input.prop("placeholder");
+                    form[id].required = $input.prop("required");
+                    form[id].persistent = $this.data("persistent");
                     form[id]["max-chars"] = $input.prop("maxlength");
                     break;
                 case 'textarea':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('textarea');
-                    form[id]["placeholder"] = $input.prop("placeholder");
-                    form[id]["required"] = $input.prop("required");
-                    form[id]["persistent"] = $this.data("persistent");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('textarea');
+                    form[id].placeholder = $input.prop("placeholder");
+                    form[id].required = $input.prop("required");
+                    form[id].persistent = $this.data("persistent");
                     break;
                 case 'range':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('input');
-                    form[id]["placeholder"] = $input.prop("placeholder");
-                    form[id]["required"] = $input.prop("required");
-                    form[id]["persistent"] = $this.data("persistent");
-                    form[id]["step"] = $input.prop("step");
-                    form[id]["min"] = $input.prop("min");
-                    form[id]["max"] = $input.prop("max");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('input');
+                    form[id].placeholder = $input.prop("placeholder");
+                    form[id].required = $input.prop("required");
+                    form[id].persistent = $this.data("persistent");
+                    form[id].step = $input.prop("step");
+                    form[id].min = $input.prop("min");
+                    form[id].max = $input.prop("max");
                     break;
                 case 'checkbox':
-                    form[id]["label"] = $this.find('legend').text();
-                    form[id]["persistent"] = $this.data("persistent");
+                    form[id].label = $this.find('legend').text();
+                    form[id].persistent = $this.data("persistent");
                     var checkboxes = [];
                     var required;
                     $this.find('input[type="checkbox"]').each(function () {
@@ -403,14 +404,14 @@ class Convertor {
                         checkboxes.push(ch);
                         required = (0, _jquery2.default)(this).attr("required");
                     });
-                    form[id]["required"] = required;
-                    form[id]["checkboxes"] = checkboxes;
+                    form[id].required = required;
+                    form[id].checkboxes = checkboxes;
                     break;
                 case 'radio':
-                    form[id]["label"] = $this.find('legend').text();
-                    form[id]["persistent"] = $this.data("persistent");
+                    form[id].label = $this.find('legend').text();
+                    form[id].persistent = $this.data("persistent");
                     var radios = [];
-                    var required;
+                    required = undefined;
                     $this.find('input[name="' + id + '"]').each(function (event) {
                         var rd;
                         var $img = (0, _jquery2.default)(this).prev().find('img');
@@ -424,54 +425,54 @@ class Convertor {
                         radios.push(rd);
                         required = (0, _jquery2.default)(this).attr("required");
                     });
-                    form[id]["required"] = required;
-                    form[id]["radios"] = radios;
+                    form[id].required = required;
+                    form[id].radios = radios;
                     break;
                 case 'select':
-                    form[id]["label"] = $this.find('legend').text();
-                    form[id]["required"] = $this.find('select').prop("required");
+                    form[id].label = $this.find('legend').text();
+                    form[id].required = $this.find('select').prop("required");
                     var options = [];
                     $this.find('option').each(function (event) {
                         options.push((0, _jquery2.default)(this).val());
                     });
-                    form[id]["options"] = options;
+                    form[id].options = options;
                     break;
                 case 'dtree':
-                    form[id]["label"] = $this.find('label').text();
-                    form[id]["filename"] = $this.find('input[type="hidden"]').data('dtree');
+                    form[id].label = $this.find('label').text();
+                    form[id].filename = $this.find('input[type="hidden"]').data('dtree');
                     break;
                 case 'image':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('input');
-                    form[id]["required"] = $input.prop("required");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('input');
+                    form[id].required = $input.prop("required");
                     form[id]["multi-image"] = false;
-                    form[id]["los"] = $input.attr('class') === 'camera-va';
+                    form[id].los = $input.attr('class') === 'camera-va';
                     break;
                 case 'multiimage':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('input');
-                    form[id]["required"] = $input.prop("required");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('input');
+                    form[id].required = $input.prop("required");
                     form[id]["multi-image"] = true;
-                    form[id]["los"] = $input.attr('class') === 'camera-va';
+                    form[id].los = $input.attr('class') === 'camera-va';
                     break;
                 case 'audio':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('input');
-                    form[id]["required"] = $input.prop("required");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('input');
+                    form[id].required = $input.prop("required");
                     break;
                 case 'gps':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('input');
-                    form[id]["required"] = $input.prop("required");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('input');
+                    form[id].required = $input.prop("required");
                     form[id]["gps-background"] = html.find('input[name="gps-background"]').is(':checked');
                     break;
                 case 'warning':
-                    form[id]["label"] = $this.find('label').text();
-                    var $input = $this.find('textarea');
-                    form[id]["placeholder"] = $input.prop("placeholder");
+                    form[id].label = $this.find('label').text();
+                    $input = $this.find('textarea');
+                    form[id].placeholder = $input.prop("placeholder");
                     break;
                 case 'section':
-                    form[id]["label"] = $this.find('h3').text();
+                    form[id].label = $this.find('h3').text();
                     break;
                 case undefined:
                     break;
