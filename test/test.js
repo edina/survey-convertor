7b1be292fa80ea1935dbd6d2f3f3c1bd38e64c7f
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-12-16 17:28:05 +0000

    Update the JSON to HTML (.edtr) conversion to the new format
    Issue edina/survey-designer#7

--- test/test.js
+++ test/test.js
@@ -356,6 +356,23 @@ describe('surveyConvertor#HTMLtoJSON', function() {
         assert.deepEqual(
             convertor.HTMLtoJSON(editor), JSON.parse(expected));
     });
+});
 
 
+describe('surveyConvertor#JSONtoHTML', function() {
+    var convertor;
+    beforeEach(function() {
+        convertor = new Convertor();
+    });
+
+    var editor = fs.readFileSync('test/test.edtr').toString('utf8');
+    var jsonText = fs.readFileSync('test/test.json').toString('utf8');
+
+    it('test JSON to .edtr conversion', function(){
+        var htmlLines = convertor.JSONtoHTML(JSON.parse(jsonText));
+        assert.equal(
+	    $('<html>' + htmlLines.join('') + '</html>').html(),
+	    $('<html>' + editor + '</html>').html()
+	);
+    });
 });
